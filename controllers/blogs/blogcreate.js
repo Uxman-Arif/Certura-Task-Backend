4b233bd483eef392daf1b80fc8b9f3064d892e21
Usman Arif <usmn2391@gmail.com>
2025-04-11 21:41:33 +0500

    Third Commit

--- controllers/blogs/blogcreate.js
+++ controllers/blogs/blogcreate.js
@@ -14,14 +14,15 @@ const upload = multer({ storage: storage });
 
 
 async function newblogcreate(req, res) {
-    data = req.body
-    console.log('data')
-    console.log(data)
-    await blogModel.create({title:data.title, description : data.description});
-    res.send('yes its also running from routes');
+    data = req.body;
+    console.log(data);
+    console.log(data.image);
+    console.log(req.file)
+    await blogModel.create({title: data.title, description: data.description, image: req.file?.filename});
+    return res.json('Blog is Created successfully!');
 };
 
 module.exports = {
     newblogcreate,
     upload
-}
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const path = require('path');
 const blogrouter = require('./routes/blogroutes');
 
@@ -7,6 +8,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/CerturaTask');
 const app = express();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors());
 app.use('/', blogrouter);
 
 app.listen(8000, ()=>{console.log('Server is started at PORT 8000...')});
